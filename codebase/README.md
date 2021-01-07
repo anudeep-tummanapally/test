6ea77fcb5e6ba83fefb77c041a50ec0231efcadc
TummanapallyAnuraag <21244258+TummanapallyAnuraag@users.noreply.github.com>
2021-01-07 20:10:05 +0530

    Added a Hello World C program

---
+++ codebase/README.md
@@ -0,0 +1,19 @@
+# Hello World Program
+
+## Compilation on Linux
+
+This command will compile the `main.c` C file
+
+```
+gcc main.c -o runme
+```
+
+## Execution on Linux
+
+```
+./runme
+Hello World
+```
+
+
+
---
+++ codebase/main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description: Hello World Program 
+ *
+ *        Version:  1.0
+ *        Created:  07/01/21 08:06:27 PM IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *        Authors:  Anuraag, Anudeep 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+	printf("Hello World\n");
+
+	return 0;
+}
